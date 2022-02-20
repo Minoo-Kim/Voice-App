2cda0e83fd40c2f155a2a880f587a9aa85cd8582
Minoo Kim <minookim.k@gmail.com>
2022-02-19 16:28:45 -0800

    Add files via upload

--- Voice/View Controllers/CheckinViewController.swift
+++ Voice/View Controllers/CheckinViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 import Firebase
 import FirebaseFirestore
 
+class ChecklistItem {
+    var dir: String
+    var isChecked: Bool = false
+    init(dir: String){
+        self.dir = dir
+    }
+}
+
 class CheckinViewController: UIViewController {
 
     @IBOutlet weak var TableView: UITableView!
     @IBOutlet weak var SaveButton: UIButton!
-    var data = [String]()
+    
+    //var data = [String]()
+    var data: [ChecklistItem] = []
+
     // making firstore synchronous basically
     var didFinishLoading = false {
         didSet {
@@ -46,7 +57,7 @@ class CheckinViewController: UIViewController {
                     let time = document["time"] as! String
                     let res = "Give " + amount +  " of " + medicine + " to " +  patient + " at " + time
                     if(document["completed"] as! Bool == false){
-                        self.data.append(res)
+                        self.data.append(ChecklistItem(dir: res))
                     }
                 }
                 self.didFinishLoading = true
@@ -54,40 +65,52 @@ class CheckinViewController: UIViewController {
         }
     }
     @IBAction func SaveTapped(_ sender: Any) {
-        var values : [String] = []
-        let selected_indexPaths = TableView.indexPathsForSelectedRows
-        if(selected_indexPaths != nil){
-            for indexPath in selected_indexPaths! {
-                let cell = tableView(TableView, cellForRowAt: indexPath) as! CheckInTableViewCell
-                values.append(cell.TextView.text!)
+        // obtaining indexpaths of only checkmarked tasks
+        var selected_indexPaths : [IndexPath] = []
+        for indexPath in TableView.indexPathsForVisibleRows! {
+            let cell = tableView(TableView, cellForRowAt: indexPath) as! CheckInTableViewCell
+            if(cell.accessoryType == .checkmark){
+                selected_indexPaths.append(indexPath)
             }
-            
-            for val in values{
-                let sentence = val.components(separatedBy: " ")
-                let patient = sentence[5] + " " + sentence[6];
-                print(patient);
-                let db = Firestore.firestore()
-                
-                // potentially add more whereField conditions (time, date, etc) here to guarantee correct match
-                db.collection("medical").whereField("nurseUID", isEqualTo: Auth.auth().currentUser!.uid).whereField("patient", isEqualTo: patient)
-                    .getDocuments() { (querySnapshot, err) in
-                    if let err = err {
-                        print("Error getting documents: \(err)")
-                    }
-                    else{
-                        for document in querySnapshot!.documents {
-                            let docID = document.documentID;
-                            // change completed field to true
-                            print("computer has reached this code");
-                            print(document["medicine"] as! String);
-                            db.collection("medical").document(docID).updateData(["completed" : true]);
-                        }
+        }
+        
+        // get medical directions
+        var values : [String] = []
+        for indexPath in selected_indexPaths {
+            let cell = tableView(TableView, cellForRowAt: indexPath) as! CheckInTableViewCell
+            values.append(cell.TextView.text!)
+        }
+        
+        // connect to firebase to resolve task
+        for val in values{
+            let sentence = val.components(separatedBy: " ")
+            let patient = sentence[5] + " " + sentence[6];
+            print(patient);
+            let db = Firestore.firestore()
+            // potentially add more whereField conditions (time, date, etc) here to guarantee correct match
+            db.collection("medical").whereField("nurseUID", isEqualTo: Auth.auth().currentUser!.uid).whereField("patient", isEqualTo: patient)
+                .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                }
+                else{
+                    for document in querySnapshot!.documents {
+                        let docID = document.documentID;
+                        // change completed field to true
+                        print("computer has reached this code");
+                        print(document["medicine"] as! String);
+                        db.collection("medical").document(docID).updateData(["completed" : true]);
                     }
                 }
             }
         }
         transitionToNurse()
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        tableView.frame = view.bounds
+//    }
 }
 extension CheckinViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -96,23 +119,19 @@ extension CheckinViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = data[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CheckInTableViewCell
-        cell.TextView.text! = data[indexPath.row]
+        cell.TextView.text! = item.dir
+        cell.accessoryType = item.isChecked ? .checkmark : .none
         return cell
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = data[indexPath.row]
+        item.isChecked = !item.isChecked
+        self.TableView.reloadData()
     }
-    // check mark stuff but it's kinda buggy
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-//        if(TableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark){
-//            TableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
-//            TableView.deselectRow(at: indexPath, animated: true)
-//        }
-//        else{
-//            TableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
-//        }
-//    }
+     
     func transitionToNurse(){
         let nurseViewCOntroller =
         storyboard?.instantiateViewController(withIdentifier:
@@ -122,6 +141,3 @@ extension CheckinViewController: UITableViewDelegate, UITableViewDataSource {
         view.window?.makeKeyAndVisible()
     }
 }
-
-
-
